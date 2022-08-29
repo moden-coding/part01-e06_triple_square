967e4da90e785456d05374ac7d1c2bc7c25902db
modengann <99417989+modengann@users.noreply.github.com>
2022-08-29 11:47:19 -0400

    Update triple_square.py

--- src/triple_square.py
+++ src/triple_square.py
@@ -5,18 +5,7 @@ from re import A
 
 
 def main():
-    for i in range(1, 11):
-        tri = triple(i)
-        sq = square(i)
-        if tri < sq:
-            break
-        print(f"triple({i})=={tri} square({i})=={sq}")
 
-def triple(a):
-    return a*3
-
-def square(a):
-    return a*a
 
 if __name__ == "__main__":
     main()
